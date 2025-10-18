a9b6aa92c0617d63dcf08dccb82984882d4ba805
jormungandrDeathx <jormungandr.deathx@gmail.com>
2025-10-19 00:23:50 +0530

    Update endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+     path('', health_check),
     path('admin/', admin.site.urls),
     path('api/',include('api.urls'))
 ]
