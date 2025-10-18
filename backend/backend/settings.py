572f83c24e0cb1b81df8e4f33b56c3bf48385093
jormungandrDeathx <jormungandr.deathx@gmail.com>
2025-10-19 00:53:15 +0530

    update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1yl=j5nq0(01s3x0ptx#8mm0hq4yfs$df)s-es$oxt55_%@z^3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["predictpulse.onrender.com"]
 
 
 # Application definition
